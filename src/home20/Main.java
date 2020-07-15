b9aaa690527a87a35a941d433b712e4ebefe6097
Valya-java <stav.valya@gmail.com>
2020-07-15 14:02:36 +0300

    1

--- src/home20/Main.java
+++ src/home20/Main.java
@@ -1,21 +1,22 @@
 package home20;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 9, 5, 3, 4, 2, 7};
+        List list = Arrays.asList(1, 9, 5, 3, 4, 2, 7);
         int b = 11;
-        System.out.println(Arrays.toString(getIndex(arr, b)));
+        System.out.println(Arrays.toString(getIndex(list, b)));
 
     }
 
-    public static int[] getIndex(int[] array, int b) {
-        if (array.length > 1) {
+    public static int[] getIndex(List<Integer> list, int b) {
+        if (list.size() > 1) {
             int j = 1;
-            for (int i = 0; i < array.length; i++) {
-                for (int k = j; k < array.length; k++) {
-                    if ((array[i] + array[k]) == b)
+            for (int i = 0; i < list.size(); i++) {
+                for (int k = j; k < list.size(); k++) {
+                    if ((list.get(i) + list.get(k)) == b)
                         return new int[]{i, k};
                 }
                 j++;
