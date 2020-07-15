47e7872052cece24bd7a357e5c3d1efa52314c47
Valya-java <stav.valya@gmail.com>
2020-07-15 14:05:48 +0300

    1

--- src/home20/Main.java
+++ src/home20/Main.java
@@ -7,17 +7,17 @@ public class Main {
     public static void main(String[] args) {
         List list = Arrays.asList(1, 9, 5, 3, 4, 2, 7);
         int b = 11;
-        System.out.println(Arrays.toString(getIndex(list, b)));
+        System.out.println(getIndex(list, b));
 
     }
 
-    public static int[] getIndex(List<Integer> list, int b) {
+    public static List getIndex(List<Integer> list, int b) {
         if (list.size() > 1) {
             int j = 1;
             for (int i = 0; i < list.size(); i++) {
                 for (int k = j; k < list.size(); k++) {
                     if ((list.get(i) + list.get(k)) == b)
-                        return new int[]{i, k};
+                        return Arrays.asList(i,k);
                 }
                 j++;
             }
